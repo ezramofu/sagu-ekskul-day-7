6cb8a0055c2111c8d317695e877620c6e0993c72
ezramofu <91767244+ezramofu@users.noreply.github.com>
2022-04-23 11:32:55 +0900

    udpate custom function eps.22

--- Eps.16/index.php
+++ Eps.16/index.php
@@ -5,10 +5,39 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <?php 
-        
-    ?>
+    <h1>What day's today?</h1>
+    <br>
+    <p>
+        <?php 
+            $dayofweek = date("w");
+            
+            switch ($dayofweek) {
+                case 1:
+                    echo "It is Monday!";
+                break;
+                case 2:
+                    echo "It is Tuesday!";
+                break;
+                case 3:
+                    echo "It is Wednesday!";
+                break;
+                case 4:
+                    echo "It is Thursday!";
+                break;
+                case 5:
+                    echo "It is Friday!";
+                break;
+                case 6:
+                    echo "It is Saturday!";
+                break;
+                case 0:
+                    echo "It is Sunday!";
+                break;
+            }
+        ?>
+    </p>
 </body>
 </html>
